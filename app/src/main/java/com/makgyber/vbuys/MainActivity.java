28f5072dd6046d84c1271e8eb6182566d74af901
jon mereria <makgyber@gmail.com>
2020-06-27 18:50:28 +0800

    fix login process

--- app/src/main/java/com/makgyber/vbuys/MainActivity.java
+++ app/src/main/java/com/makgyber/vbuys/MainActivity.java
@@ -1,50 +1,36 @@
 package com.makgyber.vbuys;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.app.AlertDialog;
 import android.app.SearchManager;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.SearchView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.firebase.ui.auth.AuthMethodPickerLayout;
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.IdpResponse;
-import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private FirebaseAuth mAuth;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private CollectionReference productRef = db.collection("product");
     private ProductAdapter adapter;
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
-        buildMainUi();
+        user = mAuth.getCurrentUser();
     }
 
     @Override
@@ -77,79 +63,42 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-
-
     private void buildMainUi() {
         TextView welcome = findViewById(R.id.text_view_welcome);
-        if (!mAuth.getCurrentUser().getDisplayName().isEmpty()) {
-            welcome.setText("Welcome " + mAuth.getCurrentUser().getDisplayName() + "!");
-        } else if (!mAuth.getCurrentUser().getEmail().isEmpty()) {
-            welcome.setText("Welcome " + mAuth.getCurrentUser().getEmail() + "!");
-        } else if (!mAuth.getCurrentUser().getPhoneNumber().isEmpty()) {
-            welcome.setText("Welcome " + mAuth.getCurrentUser().getPhoneNumber() + "!");
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("Welcome ");
+        if (user.getDisplayName() != null) {
+            sb.append(user.getDisplayName());
+        } else if (user.getEmail() != null) {
+            sb.append(user.getEmail());
+        } else if (user.getPhoneNumber() != null) {
+            sb.append(user.getPhoneNumber());
         } else {
-            welcome.setText("Welcome stranger!");
+            sb.append("guest");
         }
 
-//        Query query = productRef.whereEqualTo("publish", true)
-//                .whereEqualTo("tindahanId", "tindahan" + mAuth.getCurrentUser().getUid());
-//
-//        FirestoreRecyclerOptions<Product> options = new FirestoreRecyclerOptions.Builder<Product>()
-//                .setQuery(query, Product.class)
-//                .build();
-//
-//        adapter = new ProductAdapter(options);
-//
-//        RecyclerView recyclerView = findViewById(R.id.rv_products);
-//        recyclerView.setHasFixedSize(true);
-//        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-//        recyclerView.setAdapter(adapter);
-
-    }
-
-
-    @Override
-    public void onStop() {
-        super.onStop();
-//        adapter.stopListening();
+        sb.append("!");
+        welcome.setText(sb.toString());
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+
         if (mAuth.getCurrentUser() == null) {
             sendToLogin();
+        } else {
+            buildMainUi();
         }
     }
 
     private void sendToLogin() {
-        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        startActivity(new Intent(MainActivity.this, SignInActivity.class));
         finish();
     }
+
+
 }
 
-    //    @Override
-//    public void onBackPressed() {
-//
-//
-//        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-//        builder.setMessage("Exit the application?")
-//                .setCancelable(false)
-//                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-//                    @Override
-//                    public void onClick(DialogInterface dialog, int which) {
-//                        finish();
-//                    }
-//                })
-//                .setNegativeButton("No", new DialogInterface.OnClickListener() {
-//                    @Override
-//                    public void onClick(DialogInterface dialog, int which) {
-//                        dialog.cancel();
-//                    }
-//                });
-//
-//        AlertDialog alertDialog = builder.create();
-//        alertDialog.show();
-//
-//    }
 
--- app/src/main/java/com/makgyber/vbuys/SignInActivity.java
+++ app/src/main/java/com/makgyber/vbuys/SignInActivity.java
@@ -6,34 +6,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.ui.idp.SingleSignInActivity;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class LoginActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity {
 
     List<AuthUI.IdpConfig> providers;
 
     private final static int RC_SIGN_IN = 5;
-    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private FirebaseAuth mAuth;
     FirebaseAuth.AuthStateListener mAuthStateListener;
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_in);
+
+        mAuth = FirebaseAuth.getInstance();
 
         mAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 if (firebaseAuth.getCurrentUser() != null) {
-                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    // do nothing
+                    startActivity(new Intent(SignInActivity.this, MainActivity.class));
+                    finish();
                 } else {
                     setupSignInUI();
                 }
@@ -45,18 +53,19 @@ public class LoginActivity extends AppCompatActivity {
         // Choose authentication providers
         providers = Arrays.asList(
                 new AuthUI.IdpConfig.PhoneBuilder().build()
-//                , new AuthUI.IdpConfig.EmailBuilder().build()
+                , new AuthUI.IdpConfig.EmailBuilder().build()
+                , new AuthUI.IdpConfig.GoogleBuilder().build()
         );
 
         startActivityForResult(
                 AuthUI.getInstance()
                         .createSignInIntentBuilder()
                         .setAvailableProviders(providers)
-                        .setTheme(R.style.NoActionBar)
+                        .setTheme(R.style.AppTheme)
                         .setLogo(R.drawable.villagebuys_logo)
                         .setTosAndPrivacyPolicyUrls("https://www.google.com", "https://www.yahoo.com")
-                        .setAlwaysShowSignInMethodScreen(true)
-                        .setIsSmartLockEnabled(false)
+//                        .setAlwaysShowSignInMethodScreen(true)
+//                        .setIsSmartLockEnabled(false)
                         .build(),
                 RC_SIGN_IN);
     }
@@ -68,9 +77,11 @@ public class LoginActivity extends AppCompatActivity {
             IdpResponse response = IdpResponse.fromResultIntent(data);
 
             if (resultCode == RESULT_OK) {
-                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                user = FirebaseAuth.getInstance().getCurrentUser();
+                startActivity(new Intent(SignInActivity.this, MainActivity.class));
+                finish();
             } else {
-                Toast.makeText(LoginActivity.this, ""+response.getError().getMessage(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(SignInActivity.this, ""+response.getError().getMessage(), Toast.LENGTH_SHORT).show();
             }
         }
 
